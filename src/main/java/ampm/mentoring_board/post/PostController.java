bd344529579151dc54a61f23785fd7cfee9df5b5
bhoon <leafloveu@naver.com>
2025-05-19 22:21:10 +0900

    step 1

--- src/main/java/ampm/mentoring_board/post/PostController.java
+++ src/main/java/ampm/mentoring_board/post/PostController.java
@@ -1,15 +1,22 @@
 package ampm.mentoring_board.post;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
+@RequiredArgsConstructor
 public class PostController {
 
     private final PostRepository postRepository;
 
-    @Autowired
-    public PostController(PostRepository postRepository) {
-        this.postRepository = postRepository;
+    // 게시글 목록 페이지
+    @GetMapping("/posts")
+    public String listPosts(Model model) {
+        List<Post> posts = postRepository.findAll();
+        model.addAttribute("posts", posts);
+        return "posts/list";
     }
 }
