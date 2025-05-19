1a83c278c992ef5367de41aa4f3ffb4984297156
bhoon <leafloveu@naver.com>
2025-05-19 18:04:48 +0900

    first commit

---
+++ src/main/java/ampm/mentoring_board/MentoringBoardApplication.java
@@ -0,0 +1,13 @@
+package ampm.mentoring_board;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MentoringBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MentoringBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ampm/mentoring_board/MentoringBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package ampm.mentoring_board;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MentoringBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
