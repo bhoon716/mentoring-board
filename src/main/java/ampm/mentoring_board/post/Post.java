d5f66bedcdd0fa62f935d3e539c2cdefd0cb6e55
bhoon <leafloveu@naver.com>
2025-05-19 22:34:08 +0900

    게시글 기능 완성본

--- src/main/java/ampm/mentoring_board/post/Post.java
+++ src/main/java/ampm/mentoring_board/post/Post.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Getter
+@Setter
 public class Post {
 
     @Id
@@ -15,28 +19,4 @@ public class Post {
     private String title;
 
     private String content;
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 }
